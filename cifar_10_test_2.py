c7a3f7c55ea0cb057b392c5082d4f3885d7f14dd
ALISURE <562282219@qq.com>
2019-10-05 22:47:50 +0800

    555/333

--- cifar_10_test_2.py
+++ cifar_10_test_2.py
@@ -317,7 +317,7 @@ class ClassierRunner(object):
 
 
 if __name__ == '__main__':
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
     """
     0: 0.8269/0.8501  # 9_class_2048_norm_count_3level_512_128_lr_1000
@@ -420,10 +420,17 @@ if __name__ == '__main__':
     2: 0.90 classier_64_2_4_0
     2: 0.90 classier_64_2_5_0
     
+    # 83.61, 11_class_1024_256_64_1600_no_32_1_l1_sum_0_555
+    2: 0.9046 classier_1024_2_0_0
+    2: 0.9066 classier_1024_2_1_0
+    2: 0.9045 classier_256_2_2_0
+    2: 0.9043 classier_256_2_3_0
+    2: 0.8594 classier_64_2_4_0
+    2: 0.8595 classier_64_2_5_0
     """
 
-    _which = 0
-    _is_l2norm = False
+    _which = 2
+    _is_l2norm = True
     _is_fine_tune = False
     _classifier_type = 2  # 0, 1, 2
 
@@ -464,7 +471,7 @@ if __name__ == '__main__':
 
     # 8
     _low_dim = [1024, 256, 64]
-    _name = "11_class_1024_256_64_1600_no_32_1_l1_sum_0_321"
+    _name = "11_class_1024_256_64_1600_no_32_1_l1_sum_0_333"
     from cifar_11_3level_no_memory_l2_sum import HCResNet as AttentionResNet
 
     _which_out = _which * 2 + (1 if _is_l2norm else 0)
